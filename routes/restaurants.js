0095aee852d61ac8d9b9f1d407dfaad971e07d09
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-02-07 00:01:15 +0500

    final

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -16,24 +16,68 @@ router.get('/', function (req, res, next) {
 
     if (req.query.sortBy) {
       if (req.query.sortBy === "ASC") {
-        collection.find().toArray();
-        // client.close();
-        res.send("OK")
+        // localhost:3000/restaurants?sortBy=ASC
+        const uri = "mongodb+srv://abubakar2000:abubakar2000@cluster0.jm8lx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        client.connect(err => {
+          const collection = client.db("restaurantdb").collection("restaurants");
+          collection.find().sort({ restaurant_id: 1 }).toArray().then(_ => {
+            console.log(_);
+            res.send(_)
+          });
+          // client.close();
+        });
       } else if (req.query.sortBy === "DESC") {
+        // localhost:3000/restaurants?sortBy=DESC
         console.log("Sort by sent desc");
+        const uri = "mongodb+srv://abubakar2000:abubakar2000@cluster0.jm8lx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        client.connect(err => {
+          const collection = client.db("restaurantdb").collection("restaurants");
+          collection.find().sort({ restaurant_id: -1 }).toArray().then(_ => {
+            console.log(_);
+            res.send(_)
+          });
+          // client.close();
+        });
       }
+    } else {
+      // localhost:3000/restaurants
+      const uri = "mongodb+srv://abubakar2000:abubakar2000@cluster0.jm8lx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+      client.connect(err => {
+        const collection = client.db("restaurantdb").collection("restaurants");
+        collection.find().sort().toArray().then(_ => {
+          console.log(_);
+          res.send(_)
+        });
+        // client.close();
+      });
     }
-    
+
   });
   // res.send(data);
 });
 
 router.get('/cuisine', (req, res) => {
-  res.send(data)
+
 });
 
 router.get('/cuisine/:cuisine', (req, res) => {
-  res.send("Cusine " + req.params.cuisine)
+
+
+  const uri = "mongodb+srv://abubakar2000:abubakar2000@cluster0.jm8lx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(err => {
+    const collection = client.db("restaurantdb").collection("restaurants");
+    collection.find(
+      { cuisine: req.params.cuisine }
+    ).toArray().then(_ => {
+      console.log(_);
+      res.send(_)
+    });
+    // client.close();
+  });
 });
 
 module.exports = router;
